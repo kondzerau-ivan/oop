1cec7d3433ceeca16a922f9c6718fa38cadbf5b3
Ivan Kondzerau <kondzerau.ivan@gmail.com>
2024-03-18 22:35:28 +0300

    8. Класс Request

--- core/Request.php
+++ core/Request.php
@@ -15,11 +15,27 @@ class Request
 
   public function getPath(): string
   {
-    return $this->uri;
+    return $this->removeQueryString();
   }
 
   public function getMethod(): string
   {
     return $_SERVER['REQUEST_METHOD'];
+  }
+
+  public function removeQueryString(): string
+  {
+    if($this->uri) {
+      $params = explode('&', $this->uri);
+      if(!str_contains($params[0], '=')) {
+        return trim($params[0], '/');
+      }
+    }
+    return '';
+  }
+
+  public function get($name, $default = null): ?string
+  {
+    return $_GET[$name] ?? $default;
   }
 }
